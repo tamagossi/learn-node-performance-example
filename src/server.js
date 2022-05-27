530b9dda50b17ae1e21636a2d657c54b9a82e969
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-05-27 09:40:46 +0700

    feat(): add comment related to pm2 command

--- src/server.js
+++ src/server.js
@@ -20,3 +20,36 @@ app.get('/timer', (req, res) => {
 app.listen(8000, () => {
 	console.log(`App running with worker ${process.pid} and listening to PORT 8000`);
 });
+
+/**
+ *
+ * Important PM2 Command:
+ *
+ * 1. pm2 start <filename> -i <number of cluster>
+ *
+ *    to start a server
+ *
+ * 	  -- args --
+ *    <filename> file name of your entry point
+ *    -i is for using cluseting
+ *    <number of cluster> you can put a number or 'max' for dividing worker
+ *
+ * 2. pm2 kill
+ *
+ *    to kill all process running
+ *
+ * 3. pm2 stop <id>
+ *
+ *    to stop an instance
+ *
+ *    args:
+ *    <id> id of running instance
+ *
+ * 4. pm2 log
+ *
+ *    to log information of running instance
+ *
+ * 5. pm2 monit
+ *
+ *    to monitor running proccesses
+ */
